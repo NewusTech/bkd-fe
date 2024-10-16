926a49fa7a607e2dcbb797ef64c118b562896e68
hariando7 <rianaja2200022@gmail.com>
2024-10-16 17:04:21 +0700

    feat: integrasi forgot password

--- src/components/pages/hero/index.tsx
+++ src/components/pages/hero/index.tsx
@@ -12,42 +12,87 @@ import { heros } from "@/constants/main";
 import Image from "next/image";
 import { CarouselSliderInterface } from "@/types/interface";
 import Autoplay from "embla-carousel-autoplay";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 export default function HeroScreen({
   slides,
 }: {
   slides: CarouselSliderInterface[];
 }) {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
-    <div className="w-full md:self-center md:flex md:justify-center md:items-center">
-      <Carousel
-        opts={{
-          loop: true,
-        }}
-        plugins={[
-          Autoplay({
-            delay: 3000,
-          }),
-        ]}
-        className="w-full">
-        <CarouselContent className="w-full">
-          {slides?.map((slide: CarouselSliderInterface, i: number) => {
-            return (
-              <CarouselItem key={i} className="w-full">
-                <div className="w-full h-full">
-                  <Image
-                    src={slide.image}
-                    alt="carousel"
-                    width={1000}
-                    height={400}
-                    className="w-full h-full object-contain"
-                  />
-                </div>
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-      </Carousel>
-    </div>
+    <>
+      {
+        !isMobile ? (
+          <>
+            <div className="w-full md:self-center md:flex md:justify-center md:items-center">
+              <Carousel
+                opts={{
+                  loop: true,
+                }}
+                plugins={[
+                  Autoplay({
+                    delay: 3000,
+                  }),
+                ]}
+                className="w-full">
+                <CarouselContent className="w-full">
+                  {slides?.map((slide: CarouselSliderInterface, i: number) => {
+                    return (
+                      <CarouselItem key={i} className="w-full">
+                        <div className="w-full h-full">
+                          <Image
+                            src={slide.image}
+                            alt="carousel"
+                            width={1000}
+                            height={400}
+                            className="w-full h-full object-contain"
+                          />
+                        </div>
+                      </CarouselItem>
+                    );
+                  })}
+                </CarouselContent>
+              </Carousel>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="w-full md:self-center md:flex md:justify-center md:items-center">
+              <Carousel
+                opts={{
+                  loop: true,
+                }}
+                plugins={[
+                  Autoplay({
+                    delay: 3000,
+                  }),
+                ]}
+                className="w-full">
+                <CarouselContent className="w-full">
+                  {slides?.map((slide: CarouselSliderInterface, i: number) => {
+                    return (
+                      <CarouselItem key={i} className="w-full">
+                        <div className="w-full h-full">
+                          <Image
+                            src={slide.image_potrait ?? ""}
+                            alt="carousel"
+                            width={1000}
+                            height={400}
+                            className="w-full h-full object-contain"
+                          />
+                        </div>
+                      </CarouselItem>
+                    );
+                  })}
+                </CarouselContent>
+              </Carousel>
+            </div>
+          </>
+        )
+      }
+    </>
+
+
   );
 }
